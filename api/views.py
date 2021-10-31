a7d4a6b376ff3f3653389b89c3ca49b76ef092bf
Alejandro M. Coca <alxmcr@outlook.com>
2021-10-31 14:20:11 -0400

    Views: CityList, CityDetails, FlightList, and FlightDetails

--- api/views.py
+++ api/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
 import json
 from rest_framework import generics
-from api.serializers import PersonSerializer
-from api.models import Person
+from django_filters.rest_framework import DjangoFilterBackend
+from api.serializers import CitySerializer, FlightSerializer
+from api.models import City, Flight
 
 def index(request):
     welcome_message = {'message': 'Welcome to API /v1!'}
     return HttpResponse(json.dumps(welcome_message, indent=4, sort_keys=True), content_type="application/json")
 
-class PersonList(generics.ListCreateAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+class CityList(generics.ListCreateAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'country']
+
+class CityDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+class FlightList(generics.ListCreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['date', 'flight_from', 'flight_to']
+
+class FlightDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
