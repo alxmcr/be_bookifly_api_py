1a1c210eefdc27b1bc87672ddef08a092d7660b0
Alejandro M. Coca <alxmcr@outlook.com>
2021-10-31 14:20:28 -0400

    API: Create routes for backend

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    path("v1/people", views.PersonList.as_view())
+    path("v1/cities", views.CityList.as_view()),
+    path("v1/cities/<int:pk>", views.CityDetails.as_view()),
+    path("v1/flights", views.FlightList.as_view()),
+    path("v1/flights/<int:pk>", views.FlightDetails.as_view()),
 ]
